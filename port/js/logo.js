ff2a9f719303d37ab2881512167075db9da0d41c
Paulo jorge xavier dos santos <152025930+Pjxsantos@users.noreply.github.com>
2023-12-01 19:37:58 -0300

    Update logo.js

--- port/js/logo.js
+++ port/js/logo.js
@@ -1,1 +1,27 @@
+//DRY code, shoutout to Venkat Subramaniam
+let start = document.querySelector('#brand');
+let ex = 10;
+function swing(element) {
+
+    function update(time) {
+        
+        const x = Math.sin(time / 1231) * ex;
+        const y = Math.sin(time / 1458) * ex;
+
+        element.style.transform = [
+            `rotateX(${x}deg)`,
+            `rotateY(${y}deg)`
+        ].join(' ');
+
+        requestAnimationFrame(update);
+    }
+    update(0); //love your nested functions
+}
+
+swing(start);
+
+
+let start_button = start.querySelector('a');
+let og_color = start_button.style.color;
+let inter = 0;
 
